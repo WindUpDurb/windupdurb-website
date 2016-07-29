9243c1b19e75d708d77e28007d22aea153fc1f29
David Urbina <durbina1991@gmail.com>
2016-07-28 23:28:09 -0700

    Pre-Checkout Commit

--- public/js/concat.js
+++ public/js/concat.js
@@ -162,32 +162,41 @@ app.controller("mainController", function ($scope, EssayServices, $state) {
 
 app.controller("essaysController", function ($scope, EssayServices, $state, $sce) {
     let pages;
-    let currentPage = 0;
+    $scope.currentPage = 0;
     $scope.firstPage = true;
     $scope.lastPage = false;
-    $scope.leafThroughPages = function (which) {
+    $scope.nextPage = function () {
+        console.log("Here");
+        console.log($scope.currentPage);
+        $scope.currentPage++;
+    };
+    $scope.previousPage = function() {
+        console.log("Here");
+        $scope.currentPage--;
+    };
+  /*  $scope.nextPage = function () {
         if (which === "next") {
             currentPage++;
         } else {
             currentPage--;
         }
         $scope.currentPage = pages[currentPage];
-        if(page === 0) {
+        if(currentPage === 0) {
             $scope.firstPage = true;
             $scope.lastPage = false;
-        } else if (page === pages.length) {
+        } else if (currentPage === pages.length) {
             $scope.firstPage = false;
             $scope.lastPage = true;
         } else {
             $scope.firstPage = false;
             $scope.lastPage = false;
         }
-    };
+    };*/
 
     EssayServices.getAllEssays()
         .then(function (response) {
             pages = EssayServices.paginate(response.data);
-            $scope.currentPage = pages[0];
+            $scope.currentPage = pages[$scope.currentPage];
         })
         .catch(function (error) {
             console.log("Error: ", error);
